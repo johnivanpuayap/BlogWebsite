fa05605ee09ea3d3f5cf2239bbf30b9e1f4362f9
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-22 22:51:08 +0800

    Starting Code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "<p>Hello World! </p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
